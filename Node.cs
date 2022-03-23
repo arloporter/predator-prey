926d8995ff60460fa74b7296e3eda50d4d9d791c
Arlo Rostirolla <69077257+arloporter@users.noreply.github.com>
2022-03-23 16:28:37 +1100

    updated node
    having git issues. will need to remake a project to make it work

--- Node.cs
+++ Node.cs
@@ -1,22 +1,44 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
-public class Node : MonoBehaviour
+public class Node : IComparable<Node>
 {
 
-    public int X;
-    public int Y;
+    public float X;
+    public float Y;
+    public int xMapIndex;
+    public int yMapIndex;
     public bool hasCollider;
     public Node parent;
-    public int G;
-    public int H;
-    public float Distance;
+    public Vector2 action;
+    public int cost;
+    public int heuristic;
+    public int priority;
 
-    public Node(bool hasCollider, int X, int Y) {
+    public Node(bool hasCollider, float X, float Y)
+    {
         this.hasCollider = hasCollider;
         this.X = X;
         this.Y = Y;
     }
-    
+
+    public int CompareTo(Node n)
+    {
+        if(this.priority < n.priority)
+        {
+            return -1;
+        }
+        else if (this.priority == n.priority)
+        {
+            return 0;
+        }
+        else if (this.priority > n.priority)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
 }
