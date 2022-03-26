dc7e2ec32c6bf54849c1e7705f4c025678c5c3a9
Kanika Mehra <kanmehra@deloitte.com.au>
2022-03-26 18:43:38 +1100

    added evade for the civillians

--- Games and AI Group 13 Ass1/Assets/Scripts/Uninfected Scripts/UninfectedCivillian.cs
+++ Games and AI Group 13 Ass1/Assets/Scripts/Uninfected Scripts/UninfectedCivillian.cs
@@ -4,12 +4,52 @@ using UnityEngine;
 
 public class UninfectedCivillian : MonoBehaviour
 {
+    public GameObject player;
+    public float speedMultiplier;
+    private Vector2 lastPosition;
+    private Rigidbody2D rb2d;
+    private LineRenderer lr;
+    private const float RADIUS_TO_START_SLOWING_DOWN_FROM = 7f;
 
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
+    }
 
+    void Update()
+    {
+        lastPosition = player.transform.position;
+    }
 
+    private Vector2 SeekAndArrive(Vector2 target)
+    {
+        Vector2 currentAntiCovidAgentPos = rb2d.position;
+        Vector2 desiredVelocity = target - currentAntiCovidAgentPos;
 
+        if (desiredVelocity.magnitude < RADIUS_TO_START_SLOWING_DOWN_FROM)
+        {
+            desiredVelocity *= (desiredVelocity.magnitude / RADIUS_TO_START_SLOWING_DOWN_FROM);
+            print(desiredVelocity);
+        }
 
+        desiredVelocity *= speedMultiplier;
+        return desiredVelocity;
+    }
 
+    private Vector2 OffsetPursuit()
+    {
+        Vector2 target = player.transform.position;
+        Vector2 prediction = player.transform.GetComponent<Rigidbody2D>().velocity * 2;
+        return (target + prediction);
+    }
+
+    void FixedUpdate()
+    {
+        Vector2 changeInVelocity = ((OffsetPursuit()*-1) - rb2d.velocity);
+        Vector2 predictedChangeInVelocity = SeekAndArrive(changeInVelocity);
+        rb2d.AddForce(predictedChangeInVelocity);
+    }
 
 
 
