ac21b72c33ecd02453c50c785d12e85159b216a9
Arlo Rostirolla <69077257+arloporter@users.noreply.github.com>
2022-03-27 19:50:20 +1100

    add node script

---
+++ Games and AI Group 13 Ass1/Assets/Scripts/ACA Scripts/Node.cs
@@ -0,0 +1,47 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Node : IComparable<Node>
+{
+
+    public float X;
+    public float Y;
+    public int xMapIndex;
+    public int yMapIndex;
+    public bool hasCollider;
+    public Node parent;
+    public Vector2 action;
+    public int cost;
+    public double heuristic;
+    public double priority;
+
+    public Node(bool hasCollider, float X, float Y)
+    {
+        this.hasCollider = hasCollider;
+        this.X = X;
+        this.Y = Y;
+    }
+
+    public int CompareTo(Node n)
+    {
+        this.priority = this.cost + this.heuristic;
+        n.priority = n.cost + n.heuristic;
+
+        if (this.priority < n.priority)
+        {
+            return -1;
+        }
+        else if (this.priority == n.priority)
+        {
+            return 0;
+        }
+        else if (this.priority > n.priority)
+        {
+            return 1;
+        }
+        return 0;
+    }
+
+}
