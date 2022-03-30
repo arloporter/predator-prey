8d1002167eda25385b540ad396aa4e839c6bfef8
rmit-s3485519-Daniel-Castro <s3485519@student.rmit.edu.au>
2022-03-30 14:44:51 +1100

    Kanika UIC Script Update

--- Assets/Scripts/Uninfected Scripts/UninfectedCivillian.cs
+++ Assets/Scripts/Uninfected Scripts/UninfectedCivillian.cs
@@ -7,8 +7,24 @@ public class UninfectedCivillian : MonoBehaviour
     public float maxVelocity;
     public Vector3 velocity; // Public for testing Purposes
 
+    public GameObject player;
+    public float speedMultiplier;
+    private Vector2 lastPosition;
+    private Rigidbody2D rb2d;
+    private LineRenderer lr;
+    private const float RADIUS_TO_START_SLOWING_DOWN_FROM = 7f;
+
+
+    void Start()
+    {
+        rb2d = GetComponent<Rigidbody2D>();
+        lr = GetComponent<LineRenderer>();
+    }
+
     private void Update()
     {
+        lastPosition = player.transform.position;
+
         if (velocity.magnitude > maxVelocity)
         {
             velocity = velocity.normalized * maxVelocity;
@@ -19,7 +35,42 @@ public class UninfectedCivillian : MonoBehaviour
         // this.transform.rotation = Quaternion.LookRotation(velocity);
     }
 
+    private Vector2 SeekAndArrive(Vector2 target)
+    {
+        Vector2 currentAntiCovidAgentPos = rb2d.position;
+        Vector2 desiredVelocity = target - currentAntiCovidAgentPos;
 
+
+
+        if (desiredVelocity.magnitude < RADIUS_TO_START_SLOWING_DOWN_FROM)
+        {
+            desiredVelocity *= (desiredVelocity.magnitude / RADIUS_TO_START_SLOWING_DOWN_FROM);
+            print(desiredVelocity);
+        }
+
+
+
+        desiredVelocity *= speedMultiplier;
+        return desiredVelocity;
+    }
+
+
+
+    private Vector2 OffsetPursuit()
+    {
+        Vector2 target = player.transform.position;
+        Vector2 prediction = player.transform.GetComponent<Rigidbody2D>().velocity * 2;
+        return (target + prediction);
+    }
+
+
+
+    void FixedUpdate()
+    {
+        Vector2 changeInVelocity = ((OffsetPursuit() * -1) - rb2d.velocity);
+        Vector2 predictedChangeInVelocity = SeekAndArrive(changeInVelocity);
+        rb2d.AddForce(predictedChangeInVelocity);
+    }
 
 
 
