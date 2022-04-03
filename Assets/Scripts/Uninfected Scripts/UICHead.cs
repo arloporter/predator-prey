399ac36e61c6328ed3411ad84b1c7b027a8ac27a
rmit-s3485519-Daniel-Castro <s3485519@student.rmit.edu.au>
2022-04-04 00:20:24 +1000

    Map Completion, Wander Code refined, Preparation for case by case code

--- Assets/Scripts/Uninfected Scripts/UICHead.cs
+++ Assets/Scripts/Uninfected Scripts/UICHead.cs
@@ -4,10 +4,32 @@ using UnityEngine;
 
 public class UICHead : MonoBehaviour
 {
+    // public GameObject player;
+    // public float playerSightDistance;
+
+    // private MonoBehaviour boidsBehaviour;
+    // private Rigidbody2D rb;
+
+    private void Start()
+    {
+        // rb = GetComponent<Rigidbody2D>();
+        // boidsBehaviour = GetComponent<WanderingBehaviour>();
+    }
+
     // Basic Collider code for testing purposes
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Player")
             this.gameObject.SetActive(false);
+    }
+
+    private void Update()
+    {
+        //float distance = Vector2.Distance(player.transform.position, rb.transform.position);
+        //print(distance);
+        //if(distance < playerSightDistance)
+        //{
+        //    boidsBehaviour.enabled = false;
+        //}
     }
 }
--- Assets/Scripts/Uninfected Scripts/WanderingBehaviour.cs
+++ Assets/Scripts/Uninfected Scripts/WanderingBehaviour.cs
@@ -4,6 +4,8 @@ using UnityEngine;
 
 public class WanderingBehaviour : MonoBehaviour
 {
+    public Vector2 currentVelocity;
+
     public float detectionRadius = 2f;
     public float startVelocitySpeedAvg = 2f;
 
@@ -35,55 +37,87 @@ public class WanderingBehaviour : MonoBehaviour
     // Hit Collider check, searching for nearby 2D Colliders that are both Uninfected Civillians and not themself.
     void nearbyCivillians(Vector2 centre, float radius)
     {
-        Collider2D[] hitcolliders = Physics2D.OverlapCircleAll(centre, radius);
-        
+        Collider2D[] hitcolliders = Physics2D.OverlapCircleAll(centre, radius, 1);
 
         int i = 0;
-        while (i < hitcolliders.Length)
+        int j = 0;
+        if(hitcolliders.Length > 1)
         {
-            if(hitcolliders[i].gameObject.tag == "uninfectedCivillian" && hitcolliders[i].gameObject != this.gameObject)
+            Rigidbody2D[] otherrb2D = new Rigidbody2D[hitcolliders.Length];
+            while (i < hitcolliders.Length)
             {
-                Rigidbody2D otherrb2D = hitcolliders[i].gameObject.GetComponent<Rigidbody2D>();
-                if(cohesionEnabled == true)
+                // Keep an eye on this code
+                if (hitcolliders[i].gameObject.tag == "uninfectedCivillian" && hitcolliders[i].gameObject != this.gameObject)
                 {
-                    cohesion(otherrb2D);
-                }
-                if (seperationEnabled == true)
-                {
-                    seperation(otherrb2D, seperationRadius);
-                }
-                if (alignmentEnabled == true)
-                {
-                    alignment(otherrb2D);
-                }
-                // print("Friend"); // DEBUG
+                    otherrb2D[j] = hitcolliders[i].GetComponent<Rigidbody2D>();
+                    // print(otherrb2D[j].position);
+                    j++;
+                }                    
+                i++;
             }
-            i++;
+
+            if (cohesionEnabled == true)
+            {
+                cohesion(otherrb2D);
+            }
+            if (seperationEnabled == true)
+            {
+                seperation(otherrb2D, seperationRadius);
+            }
+            if (alignmentEnabled == true)
+            {
+                alignment(otherrb2D);
+            }
         }
+        
     }
 
-    void cohesion(Rigidbody2D nearbyUninfectedRB)
+    void cohesion(Rigidbody2D[] nearbyUninfectedRB)
     {
 
     }
 
-    void seperation(Rigidbody2D nearbyUninfectedRB, float seperationRadius)
+    void seperation(Rigidbody2D[] nearbyUninfectedRB, float seperationRadius)
     {
-        Vector2 movingtowards = rb.position - nearbyUninfectedRB.position;
-        float distance = movingtowards.sqrMagnitude;
-        Vector2 direction = movingtowards / distance;
-        if(distance < seperationRadius)
+        
+        for(int i = 0; i < nearbyUninfectedRB.Length; i++)
         {
-            rb.AddForce(direction);
+            if(nearbyUninfectedRB[i] != null)
+            {
+                Vector2 movingtowards = rb.position - nearbyUninfectedRB[i].position;
+                float distance = movingtowards.sqrMagnitude;
+                Vector2 direction = movingtowards / distance;
+                if (distance < seperationRadius)
+                {
+                    rb.AddForce(direction);
+                }
+            }
         }
+
+
 
         // print(direction); DEBUG
     }
 
-    void alignment(Rigidbody2D nearbyUninfectedRB)
+    void alignment(Rigidbody2D[] nearbyUninfectedRB)
     {
         Vector2 currentVelocity = rb.velocity;
-        rb.velocity = (nearbyUninfectedRB.velocity + currentVelocity) / 2;
+        Vector2 avgVelocity = Vector2.zero;
+
+        int velocityAmnt = 0;
+
+        for(int i = 0; i < nearbyUninfectedRB.Length; i++)
+        {
+            if (nearbyUninfectedRB[i] != null)
+            {
+                avgVelocity += nearbyUninfectedRB[i].velocity;
+                velocityAmnt++;
+            }
+        }
+        
+
+        rb.velocity = avgVelocity / velocityAmnt;
+        // print(avgVelocity.normalized); // DEBUG
     }
 
     void Update()
@@ -94,8 +128,8 @@ public class WanderingBehaviour : MonoBehaviour
             nearbyCivillians(this.transform.position, detectionRadius);
             // print(nearbyUninfected); // DEBUG
         }
-        
 
+        // currentVelocity = rb.velocity; // DEBUG
 
     }
 }
