f5bb480c98bc044841de6afe94f4865dba9cdf3a
rmit-s3485519-Daniel-Castro <s3485519@student.rmit.edu.au>
2022-04-05 22:01:42 +1000

    Base Completion of Boids
    Boids flocking at it's basic is done, I'd like to make it a bit better, but as of right now, i'm pretty happy with the implementation.

--- Assets/Scripts/Uninfected Scripts/WanderingBehaviour.cs
+++ Assets/Scripts/Uninfected Scripts/WanderingBehaviour.cs
@@ -4,22 +4,31 @@ using UnityEngine;
 
 public class WanderingBehaviour : MonoBehaviour
 {
+    // DEBUG test velocity to see velocity of gameObject
     public Vector2 currentVelocity;
 
     // public float maxVelocity;
     // public float minVelocity;
 
-    public float detectionRadius = 2f;
-    public float startVelocitySpeedAvg = 2f;
+    // DEBUG test object that shows the mid point between a group of boids
+    public GameObject testObject;
 
+    // Detection radius to detect nearby boids
+    public float detectionRadius = 3f;
+    // Average starting velocity for the boid on instantiate/spawn
+    public float startVelocitySpeedAvg = 2f;
+    // Radius to start seperating boids
+    public float seperationRadius = 1f;
+
+    // Enable/Disable Boids functions
     public bool cohesionEnabled = true;
     public bool seperationEnabled = true;
     public bool alignmentEnabled = true;
 
-    public float seperationRadius;
 
-    private float nearbyUninfected = 0;
-    private int interval = 10;
+
+    // Interval between code checks
+    private int interval = 3;
     private Rigidbody2D rb;
 
     void Start()
@@ -40,34 +49,38 @@ public class WanderingBehaviour : MonoBehaviour
     // Hit Collider check, searching for nearby 2D Colliders that are both Uninfected Civillians and not themself.
     void nearbyCivillians(Vector2 centre, float radius)
     {
+        // Get all nearby colliders, includes itself... for some reason
         Collider2D[] hitcolliders = Physics2D.OverlapCircleAll(centre, radius, 1);
 
-        int i = 0;
-        int j = 0;
+        // Instantiate checking how many nearby colliders/uninfected to go through the respective loops to develop the array of colliders that ARE boids.
+        int nearbyColliders = 0;
+        int nearbyUninfected = 0;
         if(hitcolliders.Length > 1)
         {
+            // Instantiate the array of rigidbody colliders.
             Rigidbody2D[] otherrb2D = new Rigidbody2D[hitcolliders.Length];
-            while (i < hitcolliders.Length)
+            while (nearbyColliders < hitcolliders.Length)
             {
-                // Keep an eye on this code
-                if (hitcolliders[i].gameObject.tag == "uninfectedCivillian" && hitcolliders[i].gameObject != this.gameObject)
+                // Checks for nearby colliders that are uninfectedCivillians as well as not the current gameObject
+                if (hitcolliders[nearbyColliders].gameObject.tag == "uninfectedCivillian" && hitcolliders[nearbyColliders].gameObject != this.gameObject)
                 {
-                    otherrb2D[j] = hitcolliders[i].GetComponent<Rigidbody2D>();
-                    // print(otherrb2D[j].position);
-                    j++;
+                    // Puts the rigidbody into the new array.
+                    otherrb2D[nearbyUninfected] = hitcolliders[nearbyColliders].GetComponent<Rigidbody2D>();
+                    nearbyUninfected++;
                 }                    
-                i++;
+                nearbyColliders++;
             }
 
-            if (cohesionEnabled == true)
+            // Function checks to run the boids functions seperately if required.
+            if (cohesionEnabled == true && nearbyUninfected > 0)
             {
                 cohesion(otherrb2D);
             }
-            if (seperationEnabled == true)
+            if (seperationEnabled == true && nearbyUninfected > 0)
             {
                 seperation(otherrb2D, seperationRadius);
             }
-            if (alignmentEnabled == true)
+            if (alignmentEnabled == true && nearbyUninfected > 0)
             {
                 alignment(otherrb2D);
             }
@@ -77,37 +90,46 @@ public class WanderingBehaviour : MonoBehaviour
 
     void cohesion(Rigidbody2D[] nearbyUninfectedRB)
     {
-        Vector2 currentTransform = this.transform.position;
+        // First vector2 is the current transform of the object
+        Vector2 avgTransform = this.transform.position;
 
+        // Starts at 1 since there is itself
         int transformAmnt = 1;
 
+        // Goes through the array parsed in to get the average value of the transforms.
         for (int i = 0; i < nearbyUninfectedRB.Length; i++)
         {
             if (nearbyUninfectedRB[i] != null)
             {
-                currentTransform.x += nearbyUninfectedRB[i].transform.position.x;
-                currentTransform.y += nearbyUninfectedRB[i].transform.position.y;
+                avgTransform.x += nearbyUninfectedRB[i].transform.position.x;
+                avgTransform.y += nearbyUninfectedRB[i].transform.position.y;
                 transformAmnt++;
             }
         }
-        print(currentTransform);
-        currentTransform /= transformAmnt;
 
-        print(currentTransform); // DEBUG
+        // Divides the Average transforms by the transform amount to get an average/centre point between the group of boids nearby
+        avgTransform /= transformAmnt;
 
-        
+        // Then we calculate the central direction by the classic direction calculation of Vector A - Vector B and then normalizing to get a direction.
+        Vector2 centreDirection = avgTransform - (Vector2)this.transform.position;
+        testObject.transform.position = avgTransform; // DEBUG
+        // print(centreDirection.normalized); // DEBUG
+
+        // Basic addforce based on the direction given, would like to improve
+        rb.AddForce(centreDirection.normalized);
 
     }
 
     void seperation(Rigidbody2D[] nearbyUninfectedRB, float seperationRadius)
     {
-        
+        // Iterates through the array to calculate the distance between two units within the boids
+        // and if the distance < seperationRadius then an opposite force is pushed against the boid to facilitate with seperation
         for(int i = 0; i < nearbyUninfectedRB.Length; i++)
         {
             if(nearbyUninfectedRB[i] != null)
             {
                 Vector2 movingtowards = rb.position - nearbyUninfectedRB[i].position;
-                float distance = movingtowards.sqrMagnitude;
+                float distance = movingtowards.magnitude;
                 Vector2 direction = movingtowards / distance;
                 if (distance < seperationRadius)
                 {
@@ -115,18 +137,14 @@ public class WanderingBehaviour : MonoBehaviour
                 }
             }
         }
-
-
-
-        // print(direction); DEBUG
     }
 
     void alignment(Rigidbody2D[] nearbyUninfectedRB)
     {
-        // Vector2 currentVelocity = rb.velocity;
-        Vector2 avgVelocity = Vector2.zero;
+        // Similar code to Cohesion, but gets the average of the velocitys to get an average direction vector
+        Vector2 avgVelocity = rb.velocity;
 
-        int velocityAmnt = 0;
+        int velocityAmnt = 1;
 
         for(int i = 0; i < nearbyUninfectedRB.Length; i++)
         {
@@ -138,9 +156,9 @@ public class WanderingBehaviour : MonoBehaviour
         }
         avgVelocity /= velocityAmnt;
 
+        // Would like to introduce rng or maybe have alignment be a bit weaker? Snaps into place quite quickly.
         rb.velocity = (rb.velocity + avgVelocity) / 2;
 
-       
     }
 
     void Update()
@@ -149,9 +167,9 @@ public class WanderingBehaviour : MonoBehaviour
         if (Time.frameCount % interval == 0)
         {
             nearbyCivillians(this.transform.position, detectionRadius);
-            // print(nearbyUninfected); // DEBUG
         }
 
+        // Potential velocity clamping/ possibly delving with velocity rng? DEBUG
         /*
         float velocityX = Mathf.Clamp(rb.velocity.x, minVelocity, maxVelocity);
         float velocityY = Mathf.Clamp(rb.velocity.y, minVelocity, maxVelocity);
