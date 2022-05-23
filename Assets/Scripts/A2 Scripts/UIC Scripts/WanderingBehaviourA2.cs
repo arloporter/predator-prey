8aeb84aeb3ba4edb74434ac90a43a70d6ba12fbb
rmit-s3485519-Daniel-Castro <s3485519@student.rmit.edu.au>
2022-05-23 21:26:51 +1000

    more iteration, MLAgent can see obstacles

---
+++ Assets/Scripts/A2 Scripts/UIC Scripts/WanderingBehaviourA2.cs
@@ -0,0 +1,149 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class WanderingBehaviourA2 : MonoBehaviour
+{
+    // Detection radius to detect nearby boids
+    public float detectionRadius = 3f;
+
+    // Enable/Disable Boids functions
+    public bool cohesionEnabled = true;
+    public float cohesionStrength = 1;
+    public bool seperationEnabled = true;
+    public float seperationStrength = 1;
+    public bool alignmentEnabled = true;
+    public float alignmentDelay = 1; // How many runs that delay alignment, this is design to smooth out the alignment process instead of being rigid.
+
+
+
+    // Interval between code checks
+    private Rigidbody2D rb;
+    private int alignmentInterval = 0;
+
+    void Start()
+    {
+        rb = GetComponent<Rigidbody2D>();
+    }
+
+    // Hit Collider check, searching for nearby 2D Colliders that are both Uninfected Civillians and not themself.
+    void nearbyCivillians(Vector2 centre, float radius)
+    {
+        // Get all nearby colliders, includes itself... for some reason
+        Collider2D[] hitcolliders = Physics2D.OverlapCircleAll(centre, radius, 1);
+
+        // Instantiate checking how many nearby colliders/uninfected to go through the respective loops to develop the array of colliders that ARE boids.
+        int nearbyColliders = 0;
+        int nearbyUninfected = 0;
+        if (hitcolliders.Length > 1)
+        {
+            // Instantiate the array of rigidbody colliders.
+            Rigidbody2D[] otherrb2D = new Rigidbody2D[hitcolliders.Length];
+            while (nearbyColliders < hitcolliders.Length)
+            {
+                // Checks for nearby colliders that are uninfectedCivillians as well as not the current gameObject
+                if (hitcolliders[nearbyColliders].gameObject.tag == "uninfectedCivillian" && hitcolliders[nearbyColliders].gameObject != this.gameObject)
+                {
+                    // Puts the rigidbody into the new array.
+                    otherrb2D[nearbyUninfected] = hitcolliders[nearbyColliders].GetComponent<Rigidbody2D>();
+                    nearbyUninfected++;
+                }
+                nearbyColliders++;
+            }
+
+            // Function checks to run the boids functions seperately if required.
+            if (cohesionEnabled == true && nearbyUninfected > 0)
+            {
+                cohesion(otherrb2D);
+            }
+            if (seperationEnabled == true && nearbyUninfected > 0)
+            {
+                seperation(otherrb2D);
+            }
+            if (alignmentEnabled == true && nearbyUninfected > 0 && alignmentInterval >= alignmentDelay)
+            {
+                alignment(otherrb2D);
+                alignmentInterval = 0;
+            }
+            else
+            {
+                alignmentInterval++;
+            }
+        }
+
+    }
+
+    void cohesion(Rigidbody2D[] nearbyUninfectedRB)
+    {
+        // First vector2 is the current transform of the object
+        Vector2 avgTransform = Vector2.zero;
+
+        // Starts at 0 since the boid is attempting to cohese with the nearby group not-including itself
+        int transformAmnt = 0;
+
+        // Goes through the array parsed in to get the average value of the transforms.
+        for (int i = 0; i < nearbyUninfectedRB.Length; i++)
+        {
+            if (nearbyUninfectedRB[i] != null)
+            {
+                avgTransform.x += nearbyUninfectedRB[i].transform.position.x;
+                avgTransform.y += nearbyUninfectedRB[i].transform.position.y;
+                transformAmnt++;
+            }
+        }
+
+        // Divides the Average transforms by the transform amount to get an average/centre point between the group of boids nearby
+        avgTransform /= transformAmnt;
+
+        // Then we calculate the central direction by the classic direction calculation of Vector A - Vector B and then normalizing to get a direction.
+        Vector2 centreDirection = avgTransform - (Vector2)this.transform.position;
+
+        // Basic addforce based on the direction given, would like to improve
+        rb.AddForce(centreDirection.normalized * cohesionStrength);
+
+    }
+
+    void seperation(Rigidbody2D[] nearbyUninfectedRB)
+    {
+        // Iterates through the array to calculate the distance between two units within the boids
+        // and if the distance < seperationRadius then an opposite force is pushed against the boid to facilitate with seperation
+        for (int i = 0; i < nearbyUninfectedRB.Length; i++)
+        {
+            if (nearbyUninfectedRB[i] != null)
+            {
+                Vector2 movingtowards = rb.position - nearbyUninfectedRB[i].position;
+                float distance = movingtowards.magnitude;
+                float distancePower = 1 / distance;
+
+                rb.AddForce((movingtowards.normalized * distancePower) * seperationStrength);
+            }
+        }
+    }
+
+    void alignment(Rigidbody2D[] nearbyUninfectedRB)
+    {
+        // Similar code to Cohesion, but gets the average of the velocitys to get an average direction vector
+        Vector2 avgVelocity = rb.velocity;
+
+        int velocityAmnt = 1;
+
+        for (int i = 0; i < nearbyUninfectedRB.Length; i++)
+        {
+            if (nearbyUninfectedRB[i] != null)
+            {
+                avgVelocity += nearbyUninfectedRB[i].velocity;
+                velocityAmnt++;
+            }
+        }
+        avgVelocity /= velocityAmnt;
+
+        // Would like to introduce rng or maybe have alignment be a bit weaker? Snaps into place quite quickly.
+        rb.velocity = avgVelocity;
+
+    }
+
+    void FixedUpdate()
+    {
+        nearbyCivillians(this.transform.position, detectionRadius);
+    }
+}
